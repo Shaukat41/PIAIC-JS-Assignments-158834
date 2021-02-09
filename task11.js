78d666167e32ed9be44cdc1919b93f4ae269a774
Shaukat Ali <shaukat41@gmail.com>
2021-02-09 13:27:20 -0600

    Added remaining tasks in assignment 1

---
+++ task11.js
@@ -0,0 +1,6 @@
+var C = 25;
+var f = (C*9/5) + 32;
+var f2 = 70
+var c = (f2-32)*5/9;
+document.write(`${C} \u00B0C is ${f} \u00B0F <br>
+${f2} \u00B0F is ${c} \u00B0C`);
---
+++ task13.js
@@ -0,0 +1,16 @@
+var a = 10;
+++a;
+a++;
+--a;
+a--;
+document.write(`The value of a is ${a} <br> \
+The value of ++a is ${++a} <br> \
+Now the value of a is ${a} <br> \
+The value of a++ is ${a++} <br> \
+Now the value of a is ${a} <br> \
+The value of --a is ${--a} <br> \
+Now the value of a is ${a} <br> \
+The value of a-- is ${a--} <br> \
+Now the value of a is ${a} <br> \
+`)
+
