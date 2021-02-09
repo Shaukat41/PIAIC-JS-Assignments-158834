8e5b0b78e62b867a41ef95ca9ecec84384b18be0
Shaukat Ali <shaukat41@gmail.com>
2021-02-09 14:44:39 -0600

    Add files via upload

---
+++ task 7/task7.js
@@ -0,0 +1,5 @@
+document.write(`Variables names can only contain $, _, a-z letters, and 0-9 Numbers <br>
+    For example $my_1stVariable. <br>
+    Variables must begin with a letter, $,or _,. For example $name, _name, or name <br>
+    Variablesnames are case sensitive. <br>
+    Variable name should not be JS key word`)
